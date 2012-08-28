ea7750a2c47df98d8b3b1ac2dd711103e87ba1f8
Marvin Davieds <marvin.davieds@gmail.com>
2012-08-28 23:58:16 +0200

    Kommentare überarbeitet
    Die Kommentare mit den drei Anführungszeiten sind nur für die
    Dokumentation von Klassen und Funktion gedacht.
    Alle anderen Codeabschnitte werden ganz normal mit einzeiligen
    Kommentaren beschrieben und auch vor den Abschnitten die sie
    beschreiben.

--- todo.py
+++ todo.py
@@ -18,7 +18,7 @@ class ModelStack (QtGui.QStackedWidget):
 
 class FrontPage(QtGui.QWidget):
     """
-        The main operating page
+        The main operating page.
     """ 
     def __init__(self, parent):
 
@@ -29,24 +29,22 @@ class FrontPage(QtGui.QWidget):
         """
             Defines how the front page looks like
         """
+        # Group box in order to group the displayed tasks in a frame
         self.groupbox = QtGui.QGroupBox("TO-DO-LIST")
+        # GridLayout in order to place the buttons, tasks appropriately.
         self.grid = QtGui.QGridLayout()
-        """
-            Group box in order to group the displayed tasks in a frame
-            GridLayout in order to place the buttons, tasks appropriately
-        """
+
+        # Some sample tasks to show the general design concept.
         self.task1 = QtGui.QCheckBox("Do your homework", self)
         self.task2 = QtGui.QCheckBox("Play Counter-Strike till Midnight", self)
         self.task3 = QtGui.QCheckBox("Sleep till next day afternoon", self)
-        """
-            Same sample tasks to show the general design concept
-        """
+
+        # Buttons to edit your tasks.
         self.add = QtGui.QPushButton("Add")
         self.new = QtGui.QPushButton("New")
         self.cancel = QtGui.QPushButton("Cancel")
-        """
-            Buttons to edit your tasks
-        """
+
+        # All Elements are add to the Layout and placed at a certain position.
         self.grid.addWidget(self.task1, 0, 1)
         self.grid.addWidget(self.task2, 1, 1)
         self.grid.addWidget(self.task3, 2, 1)
@@ -54,16 +52,13 @@ class FrontPage(QtGui.QWidget):
         self.grid.addWidget(self.add, 4, 1)
         self.grid.addWidget(self.new, 4, 2)
         self.grid.addWidget(self.cancel, 4, 3)
-        """
-            All Elements are add to the Layout and placed at a certain position
-        """
+
         self.setLayout(self.grid)
         self.move(300, 300)
         self.show()
 
 
 def main():
-    
     app = QtGui.QApplication(sys.argv)
     ex = ModelStack()
     ex.show()
