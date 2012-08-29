b22ceb73a3f0934f356195fb4f895e24b3f60e5f
Sergej Bauer <sergej.bauer93@gmail.com>
2012-08-29 23:14:16 +0200

    Method "newEvent" implementiert
    Beschreibt, wie ein neuer Task hinzugefügt werden soll

--- todoEvents.py
+++ todoEvents.py
@@ -4,3 +4,13 @@ from PyQt4 import QtGui, QtCore
 import ServerCommunicator
 import todo
 
+main = MainWindow()
+
+def newEvent():
+    text, ok = QtGui.QInputDialog.getText(self, 'New Task',
+            'Enter the new Task:')
+
+    if ok:
+        newbox = QtGui.QCheckBox(str(text), main)
+        main.grid.addWidget(newbox, main.currentGridRow, 1)
+
