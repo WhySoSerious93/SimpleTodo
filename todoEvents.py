c5e3f3f0984827bef01e23ca9e720bfe2c8a6bc5
Sergej Bauer <sergej.bauer93@gmail.com>
2012-08-31 22:07:00 +0200

    Methode "editEvent" implementiert
    Dient zum Bearbeiten bereits vorhandener Tasks

--- todoEvents.py
+++ todoEvents.py
@@ -13,3 +13,18 @@ def newEvent(parent, currentGrid):
         newbox = QtGui.QCheckBox(str(text), parent)
         parent.grid.addWidget(newbox, currentGrid, 1)
 
+def editEvent(parent, currentGrid, layout):
+    for i in range(0, currentGrid):
+        if layout.itemAtPosition(parent, i, 1) ==
+            QtGui.QCheckBox(parent) self:
+
+            box = layout.itemAtPosition(parent, i, 1)
+
+            if box.isChecked(parent) == True:
+                text, ok = QtGui.QInputDialog.getText(parent, 'Edit Task',
+                'Edit your current Task:')
+
+            if ok:
+                box.setText(str(text))
+
+
